b831f7af46a6d26df83195aa630d526fd1158ea2
Uttmesh Shukla <uttmeshs@gmail.com>
2023-02-24 20:11:30 +0530

    initial commit

---
+++ src/component/Basics/MenuCard.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const MenuCard = ({ menuData }) => {
+	
+  return (
+	<>
+	<section className='main-card--cointainer'>
+	  {menuData.map((curElem) => {
+		const {id, name, category, image, description } = curElem;
+		return(
+			<>
+			<div className='card-container' key= {id}>
+				<div className='card'>
+				<div className='card-body'>
+					<span className="card-number card-circle subtle">{id}</span>
+					<span className='card-author subtle'>{name}</span>
+					<h2 className='card-title'> {name} </h2>
+					<span className='card-discription-subtle'>{description}</span>
+				<div className='card-read'>Read</div>
+			</div>
+			 <img src={image} alt='images' className='card-media' />
+			<span className='card-tag suntle'>Order Now</span>
+			</div>
+	 		</div>
+			 </>
+		)
+	  })
+
+	  }
+	  </section>
+	 
+	</>
+  );
+}
+
+export default MenuCard
